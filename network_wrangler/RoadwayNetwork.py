b6aa9892c4c048c63a478e5dc779ae1354899fe7
chryssac <chryssac@yahoo.com>
2019-05-08 12:03:09 -0600

    updated class code
    added parameters and initialization of class variables in constructor.
    Updated read method with logging and creation of a roadway network
    object. And implemented write method

--- network_wrangler/RoadwayNetwork.py
+++ network_wrangler/RoadwayNetwork.py
@@ -15,42 +15,59 @@ class RoadwayNetwork(object):
     '''
 
 
-    def __init__(self):
+    def __init__(self, nodes, links, shapes):
         '''
         Constructor
         '''
+        
+        #TODO: are these stored as data frames or an array of some sort?
+        self.nodes = nodes
+        self.links = links
+        self.shapes = shapes
     
-    def read(self, network):
+    def read(self, linkFile: str, nodeFile: str, shapeFile: str):
         '''
         Reads a network from the roadway network standard
         
         args:
-        network (not sure of type): 
+        linkFile: full path to the link file
+        nodeFile: full path to the node file
+        shapeFile: full path to the shape file 
         '''
             
-        #df_link = pd.read_json("../example/stpaul/link.json", lines = True, orient = 'records')
-        #print(df_link.head(24))
+        links_df = pd.read_json(linkFile)
+        nodes_df = gpd.read_file(nodeFile)
+        shapes_df = gpd.read_file(shapeFile)
         
+        WranglerLogger.info('Read %s links from %s' % (links_df.size, linkFile))
+        WranglerLogger.info('Read %s nodes from %s' % (nodes_df.size, nodeFile))
+        WranglerLogger.info('Read %s shapes from %s' % (shapes_df.size, shapeFile))
         
-        df_node = gpd.read_file("../example/stpaul/node.geojson")
-        print(df_node.head())
-        df_shape = gpd.read_file("../example/stpaul/shape.geojson")
-        print(df_shape.head())
-        
-        #TODO: now what???
+        roadway_network = RoadwayNetwork(nodes = nodes_df, links = links_df, shapes = shapes_df)
+        return roadway_network
     
-    def write(self, path = '.', fileName = None, fileFormat = ""):
+    def write(self, filename: str, path: str = '.'):
         '''
         Writes a network in the roadway network standard 
         
         args:
-        path (string): the path were the output will be saved
-        filename (string): the name prefix of the roadway file that is generated
-        fileFormat (string): the network format that is written out
+        path: the path were the output will be saved
+        filename: the name prefix of the roadway files that will be generated
         '''
-        
-        #TODO: how are we writing this? Will it be one single geojson file or several like the input?
-        # Will there be any json files? 
+
         if not os.path.exists(path):
             WranglerLogger.debug("\nPath [%s] doesn't exist; creating." % path)
             os.mkdir(path)
+           
+        #TODO: if we are storing the links, nodes and shapes as data frames this will need to change
+        links_file = open(os.path.join(path, filename + "_links.json"), 'w')
+        self.links_df.to_json(path_or_buf = links_file, orient = 'records', lines = True)
+        links_file.close()
+            
+        nodes_file = open(os.path.join(path, filename + "_nodes.geojson"), 'w')
+        self.nodes_df.to_file(nodes_file, driver='GeoJSON')
+        nodes_file.close()
+        
+        shapes_file = open(os.path.join(path, filename + "_shapes.geojson"), 'w')
+        self.shapes_df.to_file(shapes_file, driver='GeoJSON')
+        shapes_file.close()
