b2c1c4a380e67b15c77b2921c8998ef2ab8d5e31
chryssac <chryssac@yahoo.com>
2019-05-10 13:02:18 -0600

    updated constructor, read and write methods
    changed the argument passed to the constructor to a partridge feed
    object and added basic error checking and initialization of class
    variables. Updated arguments and comments of read method. Added code
    that loads a feed and creates a transit network object. Updated write
    method to only write in network standard and not cube

--- network_wrangler/TransitNetwork.py
+++ network_wrangler/TransitNetwork.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import annotations
+
 import os
 import partridge as ptg
 
@@ -12,35 +14,81 @@ class TransitNetwork(object):
     Representation of a Transit Network.
     '''
 
-    def __init__(self, params):
+    def __init__(self, feed: Feed):
         '''
         Constructor
         '''
-        self.agency = None
-        self.frequencies = None
-        self.routes = None
-        self.shapes = None
-        self.stops = None
-        self.transfers = None
-        self.trips = None
+        
+        if isinstance(feed, Feed):
+            self.agency = feed.agency
+            self.frequencies = feed.frequencies
+            self.routes = feed.routes
+            self.shapes = feed.shapes
+            self.stops = feed.stops
+            self.transfers = feed.transfers
+            self.trips = feed.trips
+        else:
+            WranglerLogger.error("Incompatible feed type. Must provide a partridge Feed object.")
     
     
-    def read(self, network):
+    def read(self, agency_file: str, frequencies_file: str, routes_file: str, 
+             shapes_file: str, stops_files: str, transfers_file: str, 
+             trips_file: str, path: str = '.') -> TransitNetwork:
         '''
         Reads a network from the transit network standard
         
+        args:
+        agency_file: the agencies file
+        frequencies_file: the frequencies file
+        routes_file: the routes file
+        shapes_file: the shapes file
+        stops_files: the stops file
+        transfers_file: the transfers file
+        trips_file: the trips file
+        path: the location of all the network standard files
         '''
         
-        #gtfs_feed = ptg.feed(network, view={'trips.txt': {'service_id': service_ids},})
+        
+        #TODO: not sure which column I should be passing for the second dictionary.
+        #I thought it might be the ID column but their examples are not consistent with that
+        #and their documentation is horrible.... Any suggestions?
+        view = {agency_file: {'service_id': None},
+                frequencies_file: {'service_id': None},
+                routes_file: {'service_id': None},
+                shapes_file: {'service_id': None},
+                stops_files: {'service_id': None},
+                transfers_file: {'service_id': None},
+                trips_file: {'service_id': None},
+                }
+        
+        
+        if not os.path.exists(path):
+            WranglerLogger.debug("\nPath [%s] doesn't exist; creating." % path)
+            os.mkdir(path)
+            
+        #TODO: do we want load_feed or load_geo_feed?
+        feed = ptg.load_feed(path, view)
+        
+        WranglerLogger.info('Read %s agencies from %s' % (feed.agency.size, agency_file))
+        WranglerLogger.info('Read %s frequencies from %s' % (feed.frequencies.size, frequencies_file))
+        WranglerLogger.info('Read %s routes from %s' % (feed.routes.size, routes_file))
+        WranglerLogger.info('Read %s shapes from %s' % (feed.shapes.size, shapes_file))
+        WranglerLogger.info('Read %s stops from %s' % (feed.stops.size, stops_files))
+        WranglerLogger.info('Read %s transfers from %s' % (feed.transfers.size, transfers_file))
+        WranglerLogger.info('Read %s trips from %s' % (feed.trips.size, trips_file))
+        
+        transit_network = TransitNetwork(feed)
+        return transit_network
     
-    def write(self, path='.', filename = None, fileFormat:"lin"):
+    
+    
+    def write(self, path: str = '.', filename: str = None) -> None:
         '''
-        Writes a network in the transit network standard and/or Cube .lin files
+        Writes a network in the transit network standard
         
         args:
-        path (string): the path were the output will be saved
-        filename (string): the name prefix of the transit file that is generated
-        fileFormat (string): the network format that is written out, use 'lin' for cube format and 'txt' for network standard
+        path: the path were the output files will be saved
+        filename: the name prefix of the transit files that are generated
         '''
         
         if not os.path.exists(path):
@@ -48,61 +96,37 @@ class TransitNetwork(object):
             os.mkdir(path)
             
         else:
-            
-            if fileFormat == "lin":
-                tansit_file = os.path.join(path, filename + "." + fileFormat)
+            file = open(os.path.join(path, filename + "_agency.txt"), 'w');
+            for agency in self.agency:
+                file.write(str(agency)+"\n")
+            file.close()
                 
-                if os.path.exists(tansit_file):
-                    print ("File [%s] exists already.  Overwrite contents? (y/n/s) " % tansit_file)
-                    response = input("")
-                    WranglerLogger.debug("response = [%s]" % response)
+            file = open(os.path.join(path, filename + "_frequencies.txt"), 'w');
+            for frequency in self.frequencies:
+                file.write(str(frequency)+"\n")
+            file.close()
                     
-                    if response == "s" or response == "S":
-                        WranglerLogger.debug("Skipping!")
-                        return
-                    
-                    if response != "Y" and response != "y":
-                        exit(0)
-                        
-                WranglerLogger.info("Writing into %s\\%s" % (path, filename))
+            file = open(os.path.join(path, filename + "_routes.txt"), 'w');
+            for route in self.routes:
+                file.write(str(route)+"\n")
+            file.close()
                 
-                file = open(tansit_file, 'w');
-                #TODO: write the file. What am i writing to the cube file again???
-                file.write(";;<<Trnbuild>>;;\n")
-                file.close()
+            file = open(os.path.join(path, filename + "_shapes.txt"), 'w');
+            for shape in self.shapes:
+                file.write(str(shape)+"\n")
+            file.close()
                 
-            if fileFormat == "txt":
-                file = open(os.path.join(path, filename + "_agency." + fileFormat), 'w');
-                for agency in self.agency:
-                    file.write(str(agency)+"\n")
-                file.close()
+            file = open(os.path.join(path, filename + "_stops.txt"), 'w');
+            for stop in self.stops:
+                file.write(str(stop)+"\n")
+            file.close()
                 
-                file = open(os.path.join(path, filename + "_frequencies." + fileFormat), 'w');
-                for frequency in self.frequencies:
-                    file.write(str(frequency)+"\n")
-                file.close()
-                    
-                file = open(os.path.join(path, filename + "_routes." + fileFormat), 'w');
-                for route in self.routes:
-                    file.write(str(route)+"\n")
-                file.close()
+            file = open(os.path.join(path, filename + "_transfers.txt"), 'w');
+            for transfer in self.transfers:
+                file.write(str(transfer)+"\n")
+            file.close()
                 
-                file = open(os.path.join(path, filename + "_shapes." + fileFormat), 'w');
-                for shape in self.shapes:
-                    file.write(str(shape)+"\n")
-                file.close()
-                
-                file = open(os.path.join(path, filename + "_stops." + fileFormat), 'w');
-                for stop in self.stops:
-                    file.write(str(stop)+"\n")
-                file.close()
-                
-                file = open(os.path.join(path, filename + "_transfers." + fileFormat), 'w');
-                for transfer in self.transfers:
-                    file.write(str(transfer)+"\n")
-                file.close()
-                
-                file = open(os.path.join(path, filename + "_trips." + fileFormat), 'w');
-                for trip in self.trips:
-                    file.write(str(trip)+"\n")
-                file.close()
+            file = open(os.path.join(path, filename + "_trips.txt"), 'w');
+            for trip in self.trips:
+                file.write(str(trip)+"\n")
+            file.close()
