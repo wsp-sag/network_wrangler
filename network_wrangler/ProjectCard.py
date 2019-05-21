cfd13c740e5d7d7fb9c93d8f88cf63c35cd78fe8
Elizabeth Sall <easall@gmail.com>
2019-05-21 07:31:10 -0700

    Update Project Card to Validate
    - Add json schema requirements.
    - Adjust logger to be relative import.
    - Eliminate whitespace
    - Opens yml

--- network_wrangler/ProjectCard.py
+++ network_wrangler/ProjectCard.py
@@ -7,7 +7,7 @@ import json
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 from jsonschema.exceptions import SchemaError
-from Logger import WranglerLogger
+from .Logger import WranglerLogger
 
 class ProjectCard(object):
     '''
@@ -18,55 +18,55 @@ class ProjectCard(object):
     def __init__(self, filename: str):
         '''
         Constructor
-        
+
         args:
         filename: the full path to project card file in YML format
         '''
-        
+
         self.dictionary = None
         
         if not filename.endswith(".yml") and  not filename.endswith(".yaml"):
             error_message = "Incompatible file extension for Project Card. Must provide a YML file"
             WranglerLogger.error(error_message)
             return None
-        
         with open (filename, 'r') as card:
-            #validate project card 
+            #validate project card
             try:
                 with open("../schemas/project_card.json") as json_file:
                     schema = json.load(json_file)
-                
-                validate(card, schema)
-                self.dictionary = yaml.safe_load(card)
-                
+
+                card_dict = yaml.safe_load(open(card,'r'))
+                validate(card_dict, schema))
+                self.dictionary = card_dict
+
             except ValidationError as exc:
                 WranglerLogger.error(exc)
-            
+
             except SchemaError as exc:
                 WranglerLogger.error(exc)
-            
+
             except yaml.YAMLError as exc:
                 WranglerLogger.error(exc)
-                
+
             finally:
                 return None
-    
-       
+
+
     def get_tags(self):
         '''
         Returns the project card's 'Tags' field
         '''
         if self.dictionary != None:
             return self.dictionary.get('Tags')
-        
+
         return None
-    
-    
-    
+
+
+
     def read(self, path_to_card: str):
         '''
         Reads a Project card.
-        
+
         args:
         path_to_card (string): the path to the project card
         '''
@@ -75,60 +75,60 @@ class ProjectCard(object):
                          'Transit Service Attribute Change': self.transit_attribute_change,
                          'New Transit Dedicated Right of Way': self.new_transit_right_of_way,
                          'Parallel Managed Lanes': self.parallel_managed_lanes}
-        
+
         try:
             method_lookup[self.dictionary.get('Category')](self.dictionary)
-            
+
         except KeyError as e:
             WranglerLogger.error(e.message())
             raise NotImplementedError('Invalid Project Card Category') from e
-    
-    
+
+
     def roadway_attribute_change(self, card: dict):
         '''
         Reads a Roadway Attribute Change card.
-        
+
         args:
         card (dictionary): the project card stored in a dictionary
         '''
         WranglerLogger.info(card.get('Category'))
-    
-    
-    
+
+
+
     def new_roadway(self, card: dict):
         '''
         Reads a New Roadway card.
-        
+
         args:
         card (dictionary): the project card stored in a dictionary
         '''
         WranglerLogger.info(card.get('Category'))
-    
-    
+
+
     def transit_attribute_change(self, card: dict):
         '''
         Reads a Transit Service Attribute Change card.
-        
+
         args:
         card (dictionary): the project card stored in a dictionary
         '''
         WranglerLogger.info(card.get('Category'))
-    
-    
+
+
     def new_transit_right_of_way(self, card: dict):
         '''
         Reads a New Transit Dedicated Right of Way card.
-        
+
         args:
         card (dictionary): the project card stored in a dictionary
         '''
         WranglerLogger.info(card.get('Category'))
-    
-    
+
+
     def parallel_managed_lanes(self, card: dict):
         '''
         Reads a Parallel Managed Lanes card.
-        
+
         args:
         card (dictionary): the project card stored in a dictionary
         '''
