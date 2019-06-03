093825fb23c980bfee9a674b5096a4b5f5e8715d
Elizabeth Sall <easall@gmail.com>
2019-06-03 16:06:39 -0700

    Update TransitNetwork
    - add default file names from dir (although we shouldn't actually need to have them at all - they aren't used)
    - removed duplication of 'path' and 'directory' in read method (selected
    path)
    - remove extra whitespace

--- network_wrangler/TransitNetwork.py
+++ network_wrangler/TransitNetwork.py
@@ -13,13 +13,19 @@ from partridge.gtfs import Feed
 class TransitNetwork(object):
     '''
     Representation of a Transit Network.
+
+    Usage:
+      import network_wrangler as wr
+      stpaul = r'/home/jovyan/work/example/stpaul'
+      tc=wr.TransitNetwork.read(path=stpaul)
+
     '''
 
     def __init__(self, feed: Feed):
         '''
         Constructor
         '''
-        
+
         if isinstance(feed, Feed):
             self.agency = feed.agency
             self.frequencies = feed.frequencies
@@ -31,14 +37,16 @@ class TransitNetwork(object):
         else:
             WranglerLogger.error("Incompatible feed type. Must provide a partridge Feed object.")
             sys.exit("Incompatible feed type. Must provide a partridge Feed object.")
-    
-    
-    def read(self, agency_file: str, frequencies_file: str, routes_file: str, 
-             shapes_file: str, stops_files: str, transfers_file: str, 
-             trips_file: str, path: str = '.') -> TransitNetwork:
+
+    @staticmethod
+    def read(path: str = '.', agency_file: str = 'agency.txt',
+             frequencies_file: str = 'frequencies.txt', routes_file: str = 'routes.txt',
+             shapes_file: str = 'shapes.txt', stops_files: str = 'stops.txt',
+             transfers_file: str = 'transfers.txt',
+             trips_file: str = 'trips.txt') -> TransitNetwork:
         '''
         Reads a network from the transit network standard
-        
+
         args:
         agency_file: the agencies file
         frequencies_file: the frequencies file
@@ -47,18 +55,15 @@ class TransitNetwork(object):
         stops_files: the stops file
         transfers_file: the transfers file
         trips_file: the trips file
-        path: the location of all the network standard files
-        '''     
-        
-        if not os.path.exists(path):
-            WranglerLogger.debug("\nPath [%s] doesn't exist; creating." % path)
-            os.mkdir(path)
-            
+        '''
+
         config = geo_config()
         #using load_feed so we can feed a config to it
+        #need to make sure we have data configured per https://github.com/remix/partridge/blob/master/partridge/config.py#L251
+        #or need to update the config 
         feed = ptg.load_feed(path, view = None, config = config)
-        
-        
+
+
         #TODO: the above seems to be reading trips, routes, frequencies and agencies. Shapes, stops and transfers are not read.
         WranglerLogger.info('Read %s agencies from %s' % (feed.agency.size, agency_file))
         WranglerLogger.info('Read %s frequencies from %s' % (feed.frequencies.size, frequencies_file))
@@ -67,56 +72,56 @@ class TransitNetwork(object):
         #WranglerLogger.info('Read %s stops from %s' % (feed.stops.size, stops_files))
         #WranglerLogger.info('Read %s transfers from %s' % (feed.transfers.size, transfers_file))
         WranglerLogger.info('Read %s trips from %s' % (feed.trips.size, trips_file))
-        
+
         transit_network = TransitNetwork(feed)
         return transit_network
-    
-    
-    
+
+
+
     def write(self, path: str = '.', filename: str = None) -> None:
         '''
         Writes a network in the transit network standard
-        
+
         args:
         path: the path were the output files will be saved
         filename: the name prefix of the transit files that are generated
         '''
-        
+
         if not os.path.exists(path):
             WranglerLogger.debug("\nPath [%s] doesn't exist; creating." % path)
             os.mkdir(path)
-            
-        
+
+
         file = open(os.path.join(path, filename + "_agency.txt"), 'w');
         for agency in self.agency:
             file.write(str(agency)+"\n")
         file.close()
-                
+
         file = open(os.path.join(path, filename + "_frequencies.txt"), 'w');
         for frequency in self.frequencies:
             file.write(str(frequency)+"\n")
         file.close()
-                    
+
         file = open(os.path.join(path, filename + "_routes.txt"), 'w');
         for route in self.routes:
             file.write(str(route)+"\n")
         file.close()
-                
+
         file = open(os.path.join(path, filename + "_shapes.txt"), 'w');
         for shape in self.shapes:
             file.write(str(shape)+"\n")
         file.close()
-                
+
         file = open(os.path.join(path, filename + "_stops.txt"), 'w');
         for stop in self.stops:
             file.write(str(stop)+"\n")
         file.close()
-                
+
         file = open(os.path.join(path, filename + "_transfers.txt"), 'w');
         for transfer in self.transfers:
             file.write(str(transfer)+"\n")
         file.close()
-                
+
         file = open(os.path.join(path, filename + "_trips.txt"), 'w');
         for trip in self.trips:
             file.write(str(trip)+"\n")
