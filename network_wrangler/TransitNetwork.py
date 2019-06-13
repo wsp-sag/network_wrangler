28b16b8132772fd09719db003822f6bc7f158a76
Elizabeth Sall <easall@gmail.com>
2019-06-12 17:17:50 -0700

    More concise version of TransitNetwork
    Still fails b/c need to update feed files.

--- network_wrangler/TransitNetwork.py
+++ network_wrangler/TransitNetwork.py
@@ -4,11 +4,12 @@
 from __future__ import annotations
 
 import os, sys
+import networkx as nx
 import partridge as ptg
+from partridge.config import geo_config, default_config
+from partridge.gtfs import Feed
 
 from .Logger import WranglerLogger
-from partridge.config import geo_config
-from partridge.gtfs import Feed
 
 class TransitNetwork(object):
     '''
@@ -21,108 +22,65 @@ class TransitNetwork(object):
 
     '''
 
-    def __init__(self, feed: Feed):
+    def __init__(self, feed_path: str = None):
         '''
         Constructor
+
+        Reads a network from the transit network standard
         '''
 
-        if isinstance(feed, Feed):
-            self.agency = feed.agency
-            self.frequencies = feed.frequencies
-            self.routes = feed.routes
-            self.shapes = feed.shapes
-            self.stops = feed.stops
-            self.transfers = feed.transfers
-            self.trips = feed.trips
-        else:
-            WranglerLogger.error("Incompatible feed type. Must provide a partridge Feed object.")
-            sys.exit("Incompatible feed type. Must provide a partridge Feed object.")
+        self.config, self.feed  = TransitNetwork.read_feed(feed_path)
 
     @staticmethod
-    def read(path: str = '.', agency_file: str = 'agency.txt',
-             frequencies_file: str = 'frequencies.txt', routes_file: str = 'routes.txt',
-             shapes_file: str = 'shapes.txt', stops_files: str = 'stops.txt',
-             transfers_file: str = 'transfers.txt',
-             trips_file: str = 'trips.txt') -> TransitNetwork:
+    def validate_feed(feed: Feed, config: nx.DiGraph):
         '''
-        Reads a network from the transit network standard
-
-        args:
-        agency_file: the agencies file
-        frequencies_file: the frequencies file
-        routes_file: the routes file
-        shapes_file: the shapes file
-        stops_files: the stops file
-        transfers_file: the transfers file
-        trips_file: the trips file
+        Since Partridge lazily loads the df, this will
         '''
+        try:
+            for node in config.nodes.keys():
+                feed.get(node)
+            return True
+        except AttributeError:
+            return False
 
+    @staticmethod
+    def read_feed(feed_path: str = None):
         config = geo_config()
-        #using load_feed so we can feed a config to it
-        #need to make sure we have data configured per https://github.com/remix/partridge/blob/master/partridge/config.py#L251
-        #or need to update the config 
-        feed = ptg.load_feed(path, view = None, config = config)
+        config.nodes['shapes.txt']['required_columns'] = config.nodes['shapes.txt']['required_columns']+('A','B','LINK_ID')
 
+        try:
+            feed = ptg.load_feed(feed_path, config = config)
+            TransitNetwork.validate_feed(feed,config)
 
-        #TODO: the above seems to be reading trips, routes, frequencies and agencies. Shapes, stops and transfers are not read.
-        WranglerLogger.info('Read %s agencies from %s' % (feed.agency.size, agency_file))
-        WranglerLogger.info('Read %s frequencies from %s' % (feed.frequencies.size, frequencies_file))
-        WranglerLogger.info('Read %s routes from %s' % (feed.routes.size, routes_file))
-        #WranglerLogger.info('Read %s shapes from %s' % (feed.shapes.size, shapes_file))
-        #WranglerLogger.info('Read %s stops from %s' % (feed.stops.size, stops_files))
-        #WranglerLogger.info('Read %s transfers from %s' % (feed.transfers.size, transfers_file))
-        WranglerLogger.info('Read %s trips from %s' % (feed.trips.size, trips_file))
+        except KeyError:
+            config = default_config()
+            config.nodes['shapes.txt']['required_columns'] = ('shape_id','A','B','LINK_ID')
 
-        transit_network = TransitNetwork(feed)
-        return transit_network
+            WranglerLogger.warning("Reducing data requirements for shapes.txt to:", config.nodes['shapes.txt']['required_columns'])
+            feed = ptg.load_feed(feed_path, config = config)
+            TransitNetwork.validate_feed(feed,config)
 
+        ## todo should be read in as a schema
+        WranglerLogger.info('Read %s agencies from %s' % (feed.agency.size, os.path.join(feed_path,"agency.txt")))
+        WranglerLogger.info('Read %s frequencies from %s' % (feed.frequencies.size, os.path.join(feed_path,"frequencies.txt")))
+        WranglerLogger.info('Read %s routes from %s' % (feed.routes.size, os.path.join(feed_path,"routes.txt")))
+        #WranglerLogger.info('Read %s shapes from %s' % (feed.shapes.size, os.path.join(feed_path,"shapes.txt")))
+        WranglerLogger.info('Read %s stops from %s' % (feed.stops.size, os.path.join(feed_path,"stops.txt")))
+        WranglerLogger.info('Read %s transfers from %s' % (feed.transfers.size, os.path.join(feed_path,"transfers.txt")))
+        WranglerLogger.info('Read %s trips from %s' % (feed.trips.size, os.path.join(feed_path,"transfers.txt")))
 
+        return config, feed
 
-    def write(self, path: str = '.', filename: str = None) -> None:
+    def write(self, outpath: str = '.') -> None:
         '''
         Writes a network in the transit network standard
 
         args:
-        path: the path were the output files will be saved
-        filename: the name prefix of the transit files that are generated
+        outpath: the path were the output files will be saved
         '''
 
-        if not os.path.exists(path):
-            WranglerLogger.debug("\nPath [%s] doesn't exist; creating." % path)
-            os.mkdir(path)
-
-
-        file = open(os.path.join(path, filename + "_agency.txt"), 'w');
-        for agency in self.agency:
-            file.write(str(agency)+"\n")
-        file.close()
-
-        file = open(os.path.join(path, filename + "_frequencies.txt"), 'w');
-        for frequency in self.frequencies:
-            file.write(str(frequency)+"\n")
-        file.close()
-
-        file = open(os.path.join(path, filename + "_routes.txt"), 'w');
-        for route in self.routes:
-            file.write(str(route)+"\n")
-        file.close()
-
-        file = open(os.path.join(path, filename + "_shapes.txt"), 'w');
-        for shape in self.shapes:
-            file.write(str(shape)+"\n")
-        file.close()
-
-        file = open(os.path.join(path, filename + "_stops.txt"), 'w');
-        for stop in self.stops:
-            file.write(str(stop)+"\n")
-        file.close()
-
-        file = open(os.path.join(path, filename + "_transfers.txt"), 'w');
-        for transfer in self.transfers:
-            file.write(str(transfer)+"\n")
-        file.close()
-
-        file = open(os.path.join(path, filename + "_trips.txt"), 'w');
-        for trip in self.trips:
-            file.write(str(trip)+"\n")
-        file.close()
+        for node in self.config.nodes.keys():
+            df = self.feed.get(node)
+            if not df.empty:
+                path = os.path.join(outpath, node)
+                df.to_csv(path, index=False)
--- tests/test_transit.py
+++ tests/test_transit.py
@@ -1,10 +1,16 @@
 import os
 import json
 import pytest
-from network_wrangler import TransitNetwork
+from network_wrangler import TransitNetwork, setupLogging
 
 base_path    = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 examples_dir = os.path.join(base_path,'example')
+tests_temp_dir = os.path.join(base_path,'tests','temp')
+
+if not os.path.exists(tests_temp_dir):
+    os.mkdir(tests_temp_dir)
+
+setupLogging(logFileName = os.path.join(tests_temp_dir,'network_wrangler.log'))
 
 """
 Run just the tests labeled transit using `pytest -v -m transit`
@@ -17,9 +23,9 @@ def test_transit_read_write():
 
     transit_net_dir = os.path.join(examples_dir, "stpaul")
 
-    transit_net = TransitNetwork.read(path = transit_net_dir)
+    transit_net = TransitNetwork(feed_path = transit_net_dir)
 
-    transit_net.write(filename="tr_readwrite",path='tests')
+    transit_net.write(outpath=tests_temp_dir)
 
 if __name__ == '__main__':
     test_transit_read_write()
