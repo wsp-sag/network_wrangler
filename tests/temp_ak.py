ad0b76f627f5f62e098ed718d9845379840d5f64
Ashish Kulshrestha <ashish.kulshrestha@wsp.com>
2019-06-24 16:04:32 -0700

    temp script to run roadway feature change function

---
+++ tests/temp_ak.py
@@ -0,0 +1,46 @@
+import os
+import json
+from network_wrangler import RoadwayNetwork
+from network_wrangler import ProjectCard
+import yaml
+
+def test_roadway_feature_change():
+    dir = os.path.join(os.getcwd(),'example','stpaul')
+    shape_file = os.path.join(dir,"shape.geojson")
+    link_file = os.path.join(dir,"link.json")
+    node_file = os.path.join(dir,"node.geojson")
+
+    print(shape_file)
+    print(link_file)
+    print(node_file)
+
+    print("Reading network ...")
+    net = RoadwayNetwork.read(link_file= link_file, node_file=node_file, shape_file=shape_file)
+
+    print("Reading project card ...")
+    project_card = os.path.join(os.getcwd(),'example','stpaul','project_cards','1_simple_roadway_attribute_change.yml')
+    project_card_dict = ProjectCard(project_card).dictionary
+
+    road_dict = project_card_dict.get("Road")
+    change_dict = project_card_dict.get("Change")
+    road_id = road_dict.get("Name").split("=")[1]
+    attribute = project_card_dict.get("Attribute").upper()
+    existing_value = change_dict.get("Existing")
+    build_value = change_dict.get("Build")
+
+    print('link id: %s' % (road_id))
+    print('attribute: %s' % (attribute))
+    print('existing_value: %s' % (existing_value))
+    print('build_value: %s' % (build_value))
+
+    project_link_before = next(item for item in net.links_df['features'] if item["id"] == road_id)
+    print(project_link_before)
+
+    print("Applying project card ...")
+    result = RoadwayNetwork.apply_roadway_feauture_change(net, project_card_dict)
+
+    project_link_after = next(item for item in net.links_df['features'] if item["id"] == road_id)
+    print(project_link_after)
+
+if __name__ == "__main__":
+    test_roadway_feature_change()
