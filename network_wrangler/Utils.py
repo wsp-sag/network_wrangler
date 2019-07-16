2305750af50e8544217d1b3a226039431dd96b7a
Elizabeth Sall <easall@gmail.com>
2019-07-16 16:52:29 -0700

    add utility to write json from links file
    This is here b/c it is much more convenient to use this + geopandas than vanilla json+pandas
    
    Will also be necessary when we flatten...

--- network_wrangler/Utils.py
+++ network_wrangler/Utils.py
@@ -16,3 +16,17 @@ def point_df_to_geojson(df: pd.DataFrame, properties: list):
             feature['properties'][prop] = row[prop]
         geojson['features'].append(feature)
     return geojson
+
+def link_df_to_json(df: pd.DataFrame, properties: list):
+    '''
+    Modified from: Geoff Boeing:
+    https://geoffboeing.com/2015/10/exporting-python-data-geojson/
+    '''
+
+    json = {'type':'FeatureCollection', 'features':[]}
+    for _, row in df.iterrows():
+        feature = {}
+        for prop in properties:
+            feature[prop] = row[prop]
+        json['features'].append(feature)
+    return json
--- network_wrangler/__init__.py
+++ network_wrangler/__init__.py
@@ -7,10 +7,10 @@ from .Logger import WranglerLogger, setupLogging
 from .ProjectCard import ProjectCard
 from .RoadwayNetwork import RoadwayNetwork
 from .TransitNetwork import TransitNetwork
-from .Utils import point_df_to_geojson
+from .Utils import point_df_to_geojson,link_df_to_json
 
 __all__ = ['WranglerLogger','setupLogging', 'ProjectCard', 'RoadwayNetwork',
-           'point_df_to_geojson', 'TransitNetwork']
+           'point_df_to_geojson', 'TransitNetwork','link_df_to_json']
 
 if __name__ == '__main__':
     setupLogging(logFileName = 'network_wrangler.log')
