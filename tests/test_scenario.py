b6ef6cc518ddcbd5790684e23078295796953d3c
Ashish Kulshrestha <ashish.kulshrestha@wsp.com>
2019-07-24 15:14:43 -0700

    created scenario tests

--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -5,7 +5,8 @@ from network_wrangler import Scenario
 from network_wrangler.Logger import WranglerLogger
 
 """
-Run just the tests labeled basic using `pytest -v -m basic`
+Run just the tests labeled scenario using `pytest -v -m scenario`
+To run with print statments, use `pytest -s -m scenario`
 """
 
 @pytest.mark.scenario
@@ -16,3 +17,27 @@ def test_project_card_read():
     project_card = ProjectCard.read(in_file)
     WranglerLogger.info(project_card.__dict__)
     assert(project_card.Category == "Roadway Attribute Change")
+
+@pytest.mark.ashish
+def test_scenario_conflicts():
+    base_scenario = {}
+
+    project_cards_list = []
+    project_cards_list.append(ProjectCard.read(os.path.join(os.getcwd(),'example','stpaul','project_cards','4_test_project_card.yml')))
+    project_cards_list.append(ProjectCard.read(os.path.join(os.getcwd(),'example','stpaul','project_cards','5_test_project_card.yml')))
+    project_cards_list.append(ProjectCard.read(os.path.join(os.getcwd(),'example','stpaul','project_cards','6_test_project_card.yml')))
+
+    scen = Scenario.create_scenario(base_scenario = base_scenario, project_cards_list = project_cards_list)
+    scen.check_scenario_conflicts()
+
+@pytest.mark.ashish
+def test_scenario_corequisites():
+    base_scenario = {}
+
+    project_cards_list = []
+    project_cards_list.append(ProjectCard.read(os.path.join(os.getcwd(),'example','stpaul','project_cards','4_test_project_card.yml')))
+    project_cards_list.append(ProjectCard.read(os.path.join(os.getcwd(),'example','stpaul','project_cards','5_test_project_card.yml')))
+    project_cards_list.append(ProjectCard.read(os.path.join(os.getcwd(),'example','stpaul','project_cards','6_test_project_card.yml')))
+
+    scen = Scenario.create_scenario(base_scenario = base_scenario, project_cards_list = project_cards_list)
+    scen.check_scenario_corequisites()
