5e8e32e3a9cd032b5da073e306b9a833f7f21bb6
Ashish Kulshrestha <ashish.kulshrestha@wsp.com>
2019-07-24 15:15:30 -0700

    update to creating scenario and conflicts/corequisite checking

--- network_wrangler/Scenario.py
+++ network_wrangler/Scenario.py
@@ -12,7 +12,7 @@ class Scenario(object):
     Holds information about a scenario
     '''
 
-    def __init__(self, base_scenario: dict, project_cards: [ProjectCard] = None, prerequisite_dict: OrderedDict = None, corequisite_dict: OrderedDict = None, conflict_dict: OrderedDict = None):
+    def __init__(self, base_scenario: dict, project_cards: [ProjectCard] = None, prerequisite_dict: dict = None, corequisite_dict: dict = None, conflict_dict: dict = None):
         '''
         Constructor
 
@@ -29,60 +29,57 @@ class Scenario(object):
         self.conflicts     = conflict_dict
 
     @staticmethod
-    def create_scenario(base_scenario: dict, card_directory: str, project_card_names = [], tags: [str] = None) -> Scenario:
+    def create_scenario(base_scenario: dict, card_directory: str = '', tags: [str] = None, project_cards_list = []) -> Scenario:
         '''
-        Validates project cards with a specific tag from the specified folder and
+        Validates project cards with a specific tag from the specified folder or
+        list of user specified project cards and
         creates a scenario object with the valid project card.
 
         args:
         base_scenario: the base scenario
         tags: only project cards with these tags will be read/validated
         folder: the folder location where the project cards will be
-        project_cards_names: project names
+        project_cards_list: list of project cards to be applied
         '''
 
-        project_cards = []
+        prereq_dict   = {}
+        coreq_dict    = {}
+        conflict_dict = {}
 
-        prereq_dict   = OrderedDict()
-        coreq_dict    = OrderedDict()
-        conflict_dict = OrderedDict()
+        for project_card in project_cards_list:
+            prereq_dict[project_card.name] = project_card.dependencies['prerequisite']
+            coreq_dict[project_card.name] = project_card.dependencies['corequisite']
+            conflict_dict[project_card.name] = project_card.dependencies['conflicts']
 
-        for project_name in project_card_names:
-            card = ProjectCard.read(os.path.join(card_directory, project_name + '.yml'))
-            project_cards.append(card)
+        scenario = Scenario(base_scenario, project_cards = project_cards_list, prerequisite_dict = prereq_dict, corequisite_dict = coreq_dict, conflict_dict = conflict_dict)
 
-            prereq_dict[project_name] = card.get_dependency('prerequisite')
-            coreq_dict[project_name] = card.get_dependency('corequisite')
-            conflict_dict[project_name] = card.get_dependency('conflicts')
-
-        scenario = Scenario(base_scenario, project_cards = project_cards, prerequisite_dict = prereq_dict, corequisite_dict = coreq_dict, conflict_dict = conflict_dict)
-
-        # if card_directory:
-        #     scenario.add_project_cards(card_directory, tags = tags)
+        if card_directory:
+            scenario.add_project_cards(card_directory, tags = tags)
 
         return scenario
 
-    def add_project_cards(self, folder: str, tags: [str] = []) -> [ProjectCard]:
+    def add_project_cards(self, folder: str, tags: [str] = []):
         '''
-        Adds projects cards to a list. A folder is provided to look for project cards that have a tag matching
-        the tag that is passed to the method.
+        Adds projects cards to the scenario.
+        A folder is provided to look for project cards that have a matching tag that is passed to the method.
 
         args:
-        tags: only project cards with these tags will be validated and added to the returning list
         folder: the folder location where the project cards will be
+        tags: only project cards with these tags will be validated and added to the returning scenario
         '''
 
         for file in os.listdir(folder):
             if file.endswith(".yml") or file.endswith(".yaml"):
-                project_card = ProjectCard(os.path.join(folder, file))
+                project_card = ProjectCard.read(os.path.join(folder, file))
 
                 if project_card != None:
-                    card_tags = project_card.get_tags()
+                    card_tags = project_card.tags
 
                     if not set(tags).isdisjoint(card_tags):
-                        project_cards_list.append(project_card)
-
-        return project_cards_list
+                        self.project_cards.append(project_card)
+                        self.prerequisites.update( {project_card.name : project_card.dependencies['prerequisite']} )
+                        self.corequisites.update( {project_card.name : project_card.dependencies['corequisite']} )
+                        self.conflicts.update( {project_card.name : project_card.dependencies['conflicts']} )
 
     def check_scenario_conflicts(self):
         '''
@@ -95,15 +92,39 @@ class Scenario(object):
         conflict_dict = self.conflicts
         scenario_projects = list(conflict_dict.keys())
 
-        errorFound = False
+        error = False
         for project, conflicts in conflict_dict.items():
             if not conflicts == 'None':
                 for name in conflicts:
                     if name in scenario_projects:
                         WranglerLogger.error('Projects %s has %s as conflicting project' % (project, name))
-                        errorFound = True
+                        error = True
 
-        if errorFound:
+        if error:
             sys.exit('Conflicting project found for scenario!')
+
+        return True
+
+    def check_scenario_corequisites(self):
+        '''
+        Checks if there are any missing corequisite projects in the scenario
+        Fail if the project A specifies that project B is a corequisite and project B is not included in the scenario
+
+        Returns: boolean
+        '''
+
+        corequisite_dict = self.corequisites
+        scenario_projects = list(corequisite_dict.keys())
+
+        error = False
+        for project, coreq in corequisite_dict.items():
+            if not coreq == 'None':
+                for name in coreq:
+                    if name not in scenario_projects:
+                        WranglerLogger.error('Projects %s has %s as corequisite project which is missing for the scenario' % (project, name))
+                        error = True
+
+        if error:
+            sys.exit('Missing corequisite project found for scenario!')
 
         return True
