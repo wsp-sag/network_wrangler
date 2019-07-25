460e86bdb86785008b93a4effc28703c71b39d40
Ashish Kulshrestha <ashish.kulshrestha@wsp.com>
2019-07-25 13:20:29 -0700

    test for prerequisite sorting of project cards

--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -31,7 +31,7 @@ def test_scenario_conflicts():
     scen.check_scenario_conflicts()
 
 @pytest.mark.ashish
-def test_scenario_corequisites():
+def test_scenario_requisites():
     base_scenario = {}
 
     project_cards_list = []
@@ -40,4 +40,21 @@ def test_scenario_corequisites():
     project_cards_list.append(ProjectCard.read(os.path.join(os.getcwd(),'example','stpaul','project_cards','6_test_project_card.yml')))
 
     scen = Scenario.create_scenario(base_scenario = base_scenario, project_cards_list = project_cards_list)
-    scen.check_scenario_corequisites()
+    scen.check_scenario_requisites()
+
+@pytest.mark.topo
+def test_project_sort():
+    base_scenario = {}
+
+    project_cards_list = []
+    project_cards_list.append(ProjectCard.read(os.path.join(os.getcwd(),'example','stpaul','project_cards','4_test_project_card.yml')))
+    project_cards_list.append(ProjectCard.read(os.path.join(os.getcwd(),'example','stpaul','project_cards','5_test_project_card.yml')))
+    project_cards_list.append(ProjectCard.read(os.path.join(os.getcwd(),'example','stpaul','project_cards','6_test_project_card.yml')))
+
+    scen = Scenario.create_scenario(base_scenario = base_scenario, project_cards_list = project_cards_list)
+
+    scen.check_scenario_conflicts()
+    scen.check_scenario_requisites()
+
+    sorted_projects_cards = scen.create_ordered_project_cards()
+    print([project_card.name for project_card in sorted_projects_cards])
