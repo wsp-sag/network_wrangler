90b899290fb8bc98ec1636195e5087159ab17c18
Ashish Kulshrestha <ashish.kulshrestha@wsp.com>
2020-01-21 15:53:35 -0800

    deep copy of base scenario when creating a scenario

--- network_wrangler/Scenario.py
+++ network_wrangler/Scenario.py
@@ -36,14 +36,14 @@ class Scenario(object):
         self.base_scenario = base_scenario
 
         # if the base scenario had roadway or transit networks, use them as the basis.
-        if base_scenario.get("road_net"):
-            self.road_net = base_scenario["road_net"]
-        if base_scenario.get("transit_net"):
-            self.transit_net = base_scenario["transit_net"]
+        if self.base_scenario.get("road_net"):
+            self.road_net = copy.deepcopy(self.base_scenario["road_net"])
+        if self.base_scenario.get("transit_net"):
+            self.transit_net = copy.deepcopy(self.base_scenario["transit_net"])
 
         # if the base scenario had applied projects, add them to the list of applied
         self.applied_projects = []
-        if base_scenario.get("applied_projects"):
+        if self.base_scenario.get("applied_projects"):
             self.applied_projects = base_scenario["applied_projects"]
 
         self.project_cards = project_cards
