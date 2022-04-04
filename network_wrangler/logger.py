083ef6e64ef045a9f38c74798e05f2b65b65ec0f
Yuqi <ywang@bayareametro.gov>
2022-04-04 16:39:22 -0700

    modify default setting

--- network_wrangler/logger.py
+++ network_wrangler/logger.py
@@ -3,8 +3,9 @@
 
 # Utility functions for logging
 
-import sys
+import sys, os
 import logging
+from datetime import datetime
 
 __all__ = ["WranglerLogger", "setupLogging"]
 
@@ -12,7 +13,6 @@ WranglerLogger = logging.getLogger("WranglerLogger")
 
 
 def setupLogging(
-    level: int = None,
     info_log_filename: str = None,
     debug_log_filename: str = None,
     log_to_console: bool = True
@@ -21,7 +21,6 @@ def setupLogging(
     Sets up the WranglerLogger w.r.t. the debug file location and if logging to console.
 
     args:
-        level: the level of logging that will be recorded
         info_log_filename: the location of the log file that will get created to add the INFO log.
                            The INFO Log is terse, just gives the bare minimum of details.
         debug_log_filename: the location of the log file that will get created to add the DEBUG log.
@@ -31,21 +30,25 @@ def setupLogging(
     # clear handlers if any exist already
     WranglerLogger.handlers = []
 
-    if level is None:
-        WranglerLogger.setLevel(logging.DEBUG)
-    else:
-        WranglerLogger.setLevel(level)
+    # default is 'debug' so that debug inof won't be ignored is individual handler
+    WranglerLogger.setLevel(logging.DEBUG)
 
     FORMAT = logging.Formatter(
         "%(asctime)-15s %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S,"
     )
 
-    if info_log_filename:
-        info_log_handler = logging.StreamHandler(open(info_log_filename, "w"))
-        info_log_handler.setLevel(logging.INFO)
-        info_log_handler.setFormatter(FORMAT)
-        WranglerLogger.addHandler(info_log_handler)
+    # if info_log_name not provided, default to "network_wrangler.info.log" in current working directory
+    if not info_log_filename:
+        info_log_filename = os.path.join(
+                os.getcwd(),
+                "network_wrangler_{}.info.log".format(datetime.now().strftime("%Y_%m_%d__%H_%M_%S")),
+                )
+    info_log_handler = logging.StreamHandler(open(info_log_filename, "w"))
+    info_log_handler.setLevel(logging.INFO)
+    info_log_handler.setFormatter(FORMAT)
+    WranglerLogger.addHandler(info_log_handler)
 
+    # create debug file only when debug_log_filename is provided
     if debug_log_filename:
         debug_log_handler = logging.StreamHandler(open(debug_log_filename, "w"))
         debug_log_handler.setLevel(logging.DEBUG)
