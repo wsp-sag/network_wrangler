19082692fd14518741223648e7413acad0f52fd2
Yue Shuai <48269801+yueshuaing@users.noreply.github.com>
2024-08-09 10:14:40 -0700

    fix this issue in the input files

--- network_wrangler/models/roadway/tables.py
+++ network_wrangler/models/roadway/tables.py
@@ -105,10 +105,10 @@ class RoadLinksTable(DataFrameModel):
         add_missing_columns = True
         coerce = True
 
-    # @pa.dataframe_check
-    # def unique_ab(cls, df: pd.DataFrame) -> bool:
-    #     """Check that combination of A and B are unique."""
-    #     return ~df[["A", "B"]].duplicated()
+    @pa.dataframe_check
+    def unique_ab(cls, df: pd.DataFrame) -> bool:
+        """Check that combination of A and B are unique."""
+        return ~df[["A", "B"]].duplicated()
 
     # TODO add check that if there is managed>1 anywhere, that ML_ columns are present.
 
@@ -167,8 +167,8 @@ class RoadNodesTable(DataFrameModel):
 class RoadShapesTable(DataFrameModel):
     """Datamodel used to validate if links_df is of correct format and types."""
 
-    shape_id: Series[str] = pa.Field(unique=False)
-    shape_id_idx: Optional[Series[int]] = pa.Field(unique=False)
+    shape_id: Series[str] = pa.Field(unique=True)
+    shape_id_idx: Optional[Series[int]] = pa.Field(unique=True)
 
     geometry: GeoSeries = pa.Field()
     ref_shape_id: Optional[Series] = pa.Field(nullable=True)
