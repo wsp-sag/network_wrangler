97e223f02077da1b17a780583304836ea7e9ffab
Elizabeth Sall <easall@gmail.com>
2024-10-21 11:27:44 -0700

    Update gh workflows to update "latest" and avoid docs pull clash
    - include docs build as part of release and push to avoid a pull clash
    - update checkout/python action versions for 4 and 5
    - separate prerelease and release workflows
    - prerelease workflow pushes on any main push and wont collide with versions
    - fix typo referring to other repo
    - make pyproject.toml dependencies recursive so tests also includes docs and viz

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -9,19 +9,57 @@ def test_setup(request):
     """Create virtual environment and test that network wrangler can be installed and imported."""
     WranglerLogger.info(f"--Starting: {request.node.name}")
     WranglerLogger.debug("Creating virtual environment...")
-    subprocess.run(["python", "-m", "venv", "wranglertest"], check=True)
+    venv_name = "wranglertest"
+    subprocess.run(["python", "-m", "venv", f"{venv_name}"], check=True)
     WranglerLogger.debug("Created virtual environment.\nInstalling Wrangler...")
-    install_process = subprocess.run(["wranglertest/bin/pip", "install", "-e", "."], check=True)
+    install_process = subprocess.run([f"{venv_name}/bin/pip", "install", "-e", "."], check=True)
     WranglerLogger.debug(f"Installed Wrangler.\n{install_process.stdout}")
     pip_list_process = subprocess.run(
-        ["wranglertest/bin/pip", "list"], capture_output=True, text=True, check=False
+        [f"{venv_name}/bin/pip", "list"], capture_output=True, text=True, check=False
     )
     WranglerLogger.debug(f"Venv contents:\n{pip_list_process.stdout}")
     WranglerLogger.debug("Testing import...")
 
     # Capture output and error messages
     import_process = subprocess.run(
-        ["wranglertest/bin/python", "-c", "import network_wrangler"],
+        [f"{venv_name}/bin/python", "-c", "import network_wrangler"],
+        capture_output=True,
+        text=True,
+        check=False,
+    )
+
+    if import_process.returncode != 0:
+        WranglerLogger.error(f"Import failed with error:\n{import_process.stderr}")
+        raise subprocess.CalledProcessError(
+            import_process.returncode,
+            import_process.args,
+            output=import_process.stdout,
+            stderr=import_process.stderr,
+        )
+
+    WranglerLogger.debug("Import successful.")
+
+
+def test_setup_testingenv(request):
+    """Create virtual environment and test that network wrangler can be installed and imported."""
+    WranglerLogger.info(f"--Starting: {request.node.name}")
+    venv_name = "wranglertest"
+    WranglerLogger.debug("Creating virtual environment for testing...")
+    subprocess.run(["python", "-m", "venv", f"{venv_name}"], check=True)
+    WranglerLogger.debug("Created virtual environment.\nInstalling Wrangler...")
+    install_process = subprocess.run(
+        [f"{venv_name}/bin/pip", "install", "-e", ".[tests]"], check=True
+    )
+    WranglerLogger.debug(f"Installed Wrangler.\n{install_process.stdout}")
+    pip_list_process = subprocess.run(
+        [f"{venv_name}/bin/pip", "list"], capture_output=True, text=True, check=False
+    )
+    WranglerLogger.debug(f"Venv contents:\n{pip_list_process.stdout}")
+    WranglerLogger.debug("Testing import...")
+
+    # Capture output and error messages
+    import_process = subprocess.run(
+        [f"{venv_name}/bin/python", "-c", "import network_wrangler"],
         capture_output=True,
         text=True,
         check=False,
